56191d873a248d244b67939e5074857681b06ef1
Laurence Bautista <rorens05@gmail.com>
2022-02-13 14:43:11 +0800

    Add stocks in dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,41 +1,35 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
-  menu false
+  # menu false
 
   controller do
-    before_action :redirect_customer
+    # before_action :redirect_customer
 
-    def redirect_customer
-      redirect_to admin_users_path
-    end
+    # def redirect_customer
+    #   redirect_to admin_users_path
+    # end
 
   end
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate dashboard" do
-        h3 "Welcome"
+    
+    
+    columns do
+      column do
+        panel "Product with low stocks" do
+          ul do
+            Product.order(stocks: :desc).map do |product|
+              li link_to("#{product.name} (#{product.stocks} items)", admin_product_path(product))
+            end
+          end
+        end
       end
-    end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
 
     #   column do
     #     panel "Info" do
     #       para "Welcome to ActiveAdmin."
     #     end
     #   end
-    # end
+    end
   end # content
 end
